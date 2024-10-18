1569ff15da882eb4ab8d9eea5b08d426f72e06e7
7rah <54563510+7rah@users.noreply.github.com>
2024-10-19 02:47:44 +0800

    dbg

--- scripts/create_swap.sh
+++ scripts/create_swap.sh
@@ -1,11 +1,11 @@
 sudo fallocate -l 32G /swapfile
-sudo fallocate -l 32G /mnt/swapfile
+sudo fallocate -l 32G /mnt/swapfile-new
 
 sudo chmod 600 /swapfile
-sudo chmod 600 /mnt/swapfile
+sudo chmod 600 /mnt/swapfile-new
 
 sudo mkswap /swapfile
-sudo mkswap /mnt/swapfile
+sudo mkswap /mnt/swapfile-new
 
 sudo swapon /swapfile
-sudo swapon /mnt/swapfile
+sudo swapon /mnt/swapfile-new
